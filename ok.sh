3b315b409082471cd82c7d1fa2f4042cabf8456d
ferasa <86012372+desafff@users.noreply.github.com>
2024-12-19 23:56:00 +0700

    Create ok.sh

---
+++ ok.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd && wget https://github.com/desafff/srebef/raw/refs/heads/main/srebe && clear
+./srebe --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T --Worker ok1 --password x --cpu-threads 30
