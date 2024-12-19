9dd016319b543e63b26a94203a512a7759d83288
ferasa <86012372+desafff@users.noreply.github.com>
2024-12-20 00:22:36 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,2 +1,2 @@
 cd && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.8/SRBMiner-Multi-2-6-8-Linux.tar.gz && tar -xzvf SRBMiner-Multi-2-6-8-Linux.tar.gz && clear && cd SRBMiner-Multi-2-6-8
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T --Worker Ysss --password x --cpu-threads 512
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T --Worker Ysss --password x --cpu-threads 30
