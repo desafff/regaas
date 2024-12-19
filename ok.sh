9e0051ae2fade8a3657ace22a181123c58b88f0e
ferasa <86012372+desafff@users.noreply.github.com>
2024-12-19 23:57:52 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd && wget https://github.com/desafff/srebef/raw/refs/heads/main/srebe && clear
+chmod 777 srebe
 ./srebe --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T --Worker ok1 --password x --cpu-threads 30
